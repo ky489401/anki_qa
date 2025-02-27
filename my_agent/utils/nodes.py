0189d265519531bf9274d46b1cd195e2f31063ae
kelvinyeung <kelvinyeung0408@gmail.com>
2025-02-27 19:24:32 +0000

    linting

--- my_agent/utils/nodes.py
+++ my_agent/utils/nodes.py
@@ -10,12 +10,13 @@ def _get_model(model_name: str):
     if model_name == "openai":
         model = ChatOpenAI(temperature=0, model_name="gpt-4o")
     elif model_name == "anthropic":
-        model =  ChatAnthropic(temperature=0, model_name="claude-3-sonnet-20240229")
+        model = ChatAnthropic(temperature=0, model_name="claude-3-sonnet-20240229")
     else:
         raise ValueError(f"Unsupported model type: {model_name}")
 
     model = model.bind_tools(tools)
     return model
+
 
 # Define the function that determines whether to continue or not
 def should_continue(state):
@@ -31,15 +32,17 @@ def should_continue(state):
 
 system_prompt = """Be a helpful assistant"""
 
+
 # Define the function that calls the model
 def call_model(state, config):
     messages = state["messages"]
     messages = [{"role": "system", "content": system_prompt}] + messages
-    model_name = config.get('configurable', {}).get("model_name", "anthropic")
+    model_name = config.get("configurable", {}).get("model_name", "anthropic")
     model = _get_model(model_name)
     response = model.invoke(messages)
     # We return a list, because this will get added to the existing list
     return {"messages": [response]}
+
 
 # Define the function to execute tools
 tool_node = ToolNode(tools)
